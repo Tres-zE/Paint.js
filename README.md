1c1dee69a037e30b82fd42d05bd9f6855d1242e3
ruloDev <rulo_dev@outlook.com>
2025-10-04 22:20:47 -0600

    agregamos js

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 En este nuevo proyecto impartido por midu, hasta el momento apredí algo nuevo:
-los botones en 3d, wow, muy al estilo de windows
+los botones en 3d, wow, muy al estilo de windows.
+Estilamos más el canvas y escribimmos algo de js
--- main.js
+++ main.js
@@ -1,2 +1,43 @@
+//constants
+const MODES = {
+  DRAW: "draw",
+  ERASE: "erase",
+  RECTANGLE: "rectangle",
+  ELLIPSE: "ellipse",
+  PICKER: "picker",
+};
+//utilities
 const $ = (el) => document.querySelector(el);
 const $$ = (els) => document.querySelectorAll(els);
+
+//elements
+const $canvas = "#canvas"; //DOM element for canvas
+const ctx = $canvas.getContext("2d"); //canvas context
+
+//state
+let isDrawing = false; //flag to track drawing state
+let startX, startY;
+let lastX = 0; //last x position
+let lastY = 0; //last y position
+let mode = MODES.DRAW; //current mode
+
+//event listeners
+$canvas.addEventListener("mousedown", startDrawing);
+$canvas.addEventListener("mousemove", draw);
+$canvas.addEventListener("mouseup", stopDrawing);
+$canvas.addEventListener("mouseleave", stopDrawing);
+
+//methods
+function startDrawing(e) {
+  isDrawing = true;
+
+  const { offsetX, offsetY } = e;
+
+  //guardar coordenadas iniciales 'Destructuracion'
+  [startX, startY] = [offsetX, offsetY];
+  [lastX, lastY] = [offsetX, offsetY];
+}
+
+function draw(e) {}
+
+function stopDrawing(e) {}
